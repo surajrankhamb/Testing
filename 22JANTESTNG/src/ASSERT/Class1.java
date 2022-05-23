8fbe578edf6af5ef15a1b94601f2752c863cbb8b
hp <hp@LAPTOP-CVO18DI5>
2022-05-23 21:54:57 +0530

    Please check

---
+++ 22JANTESTNG/src/ASSERT/Class1.java
@@ -0,0 +1,15 @@
+package ASSERT;
+
+import org.testng.reporters.jq.Main;
+
+public class Class1 {
+
+	
+		
+		public static void main(String[] args) {
+			
+		System.out.println("Hello");
+		
+	}
+
+}
